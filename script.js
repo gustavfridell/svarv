e5778c7eabcbdf0a58699b84adf8e0e80520a849
Gustav Fridell <gustavfridell@me.com>
2019-10-20 17:59:06 +0200

    make controls work WIP

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ const state = {
     tracks: null,
     numberOfTracks: null,
     trackIndex: 0,
-    playButton: document.querySelector('#play-button'),
-    playButtonIcon: document.querySelector('#play-button i'),
-    prevButton: document.querySelector('#prev-button'),
-    nextButton: document.querySelector('#next-button'),
-    artwork: document.querySelector('#artwork')
+    elements: {
+        playButton: document.querySelector('#play-button'),
+        playButtonIcon: document.querySelector('#play-button i'),
+        prevButton: document.querySelector('#prev-button'),
+        nextButton: document.querySelector('#next-button'),
+        artwork: document.querySelector('#artwork'),
+        title: document.querySelector('#title'),
+        artist: document.querySelector('#artist')
+    }
 }
 
 const shuffleArray = array =>  {
@@ -24,32 +28,14 @@ const shuffleArray = array =>  {
   return array
 }
 
-const displayArtwork = url => {
-    artwork.src = url
-}
-
-const displayInfo = (artist, title) => {
-    const infoContainer = document.querySelector('#info')
-    const titleElement = infoContainer.querySelector('#title')
-    const artistElement = infoContainer.querySelector('#artist')
-    artistElement.innerText = artist
-    titleElement.innerText = title
-}
-
-const playTrack = index => {
+const startStream = index => {
     const track = state.tracks[index]
-    const { artwork_url, title, user, id } = track
-    const artworkUrl = `${artwork_url.substring(0, artwork_url.length - 9)}t500x500.jpg`
-    const artist = user.username
 
-    displayArtwork(artworkUrl)
-    displayInfo(artist, title)
-
-    SC.stream(`/tracks/${id}`).then(player => {
+    SC.stream(`/tracks/${track.id}`).then(player => {
         state.player = player
 
         state.player.play().then(() => {
-            console.log('ok')
+            state.initiated = true
         }).catch(e => {
             console.log('nä', e)
             console.dir(e)
@@ -57,22 +43,39 @@ const playTrack = index => {
     })
 }
 
-const playButtonOnclickHandler = () => {
-    const { classList } = state.playButtonIcon
+const displayTrackInfo = index => {
+    const track = state.tracks[index]
+    const { artwork_url, title, user, id } = track
+    const artworkUrl = `${artwork_url.substring(0, artwork_url.length - 9)}t500x500.jpg`
+    const artist = user.username
 
-    if (state.player.isPlaying()) {
+    state.elements.artwork.src = artworkUrl
+    state.elements.artist.innerText = artist
+    state.elements.title.innerText = title
+}
+
+const playButtonOnclickHandler = () => {
+    const { classList } = state.elements.playButtonIcon
+
+    if (state.player && state.player.isPlaying()) {
         classList.remove('fa-pause')
         classList.add('fa-play')
         state.player.pause()
     } else {
         classList.remove('fa-play')
         classList.add('fa-pause')
-        state.player.play()
+
+        if (state.player) {
+            state.player.play()
+        } else {
+            startStream(state.trackIndex)
+        }
     }
 }
 
 const changeTrack = amount => {
     let { trackIndex, numberOfTracks } = state
+
     trackIndex += amount
     if (trackIndex < 0) {
         trackIndex = numberOfTracks - 1
@@ -81,7 +84,8 @@ const changeTrack = amount => {
     }
     state.trackIndex = trackIndex
 
-    playTrack(trackIndex)
+    displayTrackInfo(trackIndex)
+    startStream(trackIndex)
 }
 
 const initialize = () => {
@@ -97,12 +101,12 @@ const initialize = () => {
             numberOfTracks: track_count
         })
 
-        playTrack(state.trackIndex)
+        displayTrackInfo(state.trackIndex)
     })
 
-    state.playButton.addEventListener('click', playButtonOnclickHandler)
-    state.prevButton.addEventListener('click', () => changeTrack(-1))
-    state.nextButton.addEventListener('click', () => changeTrack(+1))
+    state.elements.playButton.addEventListener('click', playButtonOnclickHandler)
+    state.elements.prevButton.addEventListener('click', () => changeTrack(-1))
+    state.elements.nextButton.addEventListener('click', () => changeTrack(+1))
 }
 
 initialize()
